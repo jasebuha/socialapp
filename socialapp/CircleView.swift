d89776c96376bb16e21b06e5a694d4268c4764b7
jasebuha <jasebuha@hotmail.com>
2017-03-03 12:34:30 -0800

    Completed ui of feed

--- socialapp/CircleView.swift
+++ socialapp/CircleView.swift
@@ -1,29 +1,26 @@
 //
-//  RoundBtn.swift
+//  CircleView.swift
 //  socialapp
 //
-//  Created by Jason Buhagiar on 3/2/17.
+//  Created by Jason Buhagiar on 3/3/17.
 //  Copyright © 2017 Jason Buhagiar. All rights reserved.
 //
 
 import UIKit
 
-class RoundBtn: UIButton {
+class CircleView: UIImageView {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 1.0
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        imageView?.contentMode = .scaleAspectFit
     }
     
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
         layer.cornerRadius = self.frame.width / 2
     }
-
+    
 }
