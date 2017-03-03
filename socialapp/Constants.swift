e21d989f70a955a2fc9d61b213213970c6a3def1
jasebuha <jasebuha@hotmail.com>
2017-03-02 18:18:55 -0800

    Sign in ui custom classes complete

---
+++ socialapp/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  socialapp
+//
+//  Created by Jason Buhagiar on 3/2/17.
+//  Copyright © 2017 Jason Buhagiar. All rights reserved.
+//
+
+import UIKit
+
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
