238f0853d0a5f3b8ceb4767dfb9208a37143ce59
jasebuha <jasebuha@hotmail.com>
2017-03-03 18:55:16 -0800

    Complete Firebase storage download

--- socialapp/DataService.swift
+++ socialapp/DataService.swift
@@ -10,14 +10,19 @@ import Foundation
 import Firebase
 
 let DB_BASE = FIRDatabase.database().reference()
+let STORAGE_BASE = FIRStorage.storage().reference()
 
 class DataService {
     //This creates a singleton (can be accessed throughout the app in any folder
     static let ds = DataService()
     
+    //CB references
     private var _REF_BASE = DB_BASE
     private var _REF_POSTS = DB_BASE.child("posts")
     private var _REF_USERS = DB_BASE.child("users")
+    
+    //Storage references
+    private var _REF_POST_IMAGES = STORAGE_BASE.child("post-pics")
     
     var REF_BASE: FIRDatabaseReference {
         return _REF_BASE
@@ -29,6 +34,10 @@ class DataService {
     
     var REF_USERS: FIRDatabaseReference {
         return _REF_USERS
+    }
+    
+    var REF_POST_IMAGES: FIRStorageReference {
+        return _REF_POST_IMAGES
     }
     
     func createFibaseDBuser(uid: String, userData: Dictionary<String, String>) {
